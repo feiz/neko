20b643b468065749367c0bdfaef3742a234ebc84
feiz <logiq.feiz@gmail.com>
2021-02-26 16:31:13 +0900

    ヘルプを記載

--- src/saying/index.ts
+++ src/saying/index.ts
@@ -3,6 +3,42 @@ import { Saying } from './models'
 import { evaluate } from './evaluator'
 
 const command: string = 'saying'
+const helptext = `語録コマンド \`saying\`
+
+登録した語録を組み合わせて文章を生成できるコマンドです。語録を育ててたのしくあそぼう
+
+*語録操作*
+*登録*: \`?saying create 語録名\`
+*削除*: \`?saying delete|del 語録名\`
+*一覧*: \`?saying list|ls 語録名\`
+
+*語録追加・削除*
+*語録への追加*: \`?+語録名 ワード\`
+*語録から削除*: \`?-語録名 ワード\`
+
+*語録を表示*
+\`!\`で開始した文章は語録表示として解析されます
+
+*語録からランダムに一つ表示*: \`{語録名}\` あるいは \`語録名\`
+*プレーンテキストとして表示*: \`"テキスト"\`
+
+*組み合わせ例*
+
+*入力*: \`!{誰}"は"{何}"を{どのように}"した"\`
+*出力*: \`ぼくは神をバラバラにした\`
+*入力*: \`!{５}" "{７}" "{５}\`
+*出力*: \`五月雨を あつめて速し 雛見沢\`
+
+* 語録が登録されていない、もしくは登録ワードが0件の語録を表示した場合、プレーンテキストの扱いになります
+* プレーンテキストではない半角スペースはデリミタとみなされますが、出力の際は無視されます
+  * 入力 \`!５７５ ７ ７\` は、\`{５７５}{７}{７}\` と同等の入力とみなされます。
+
+*入力*: \`!未登録ワード\`
+*出力*: \`未登録ワード\`
+
+*その他*
+*ヘルプ*: \`?saying help\`
+`
 interface botCommand {
   (args: string[]): Promise<string>;
 }
@@ -48,6 +84,7 @@ app.message(/^\?\+(?<keyword>.*) (?<word>.*)/, async ({ context, say }) => {
   await Saying.add(keyword, word)
   await say(`${keyword}語録に「${word}」を登録しました。`)
 })
+
 app.message(/^\?-(?<keyword>.*) (?<word>.*)/, async ({ context, say }) => {
   const keyword = context.matches[1]
   const word = context.matches[2]
@@ -68,4 +105,8 @@ app.message(/^!(.*)/, async ({ context, say }) => {
   } catch (e) {
     await say(`${e}`)
   }
+})
+
+app.message(/^\?saying help$/, async ({context, say}) => {
+  await say(helptext)
 })
