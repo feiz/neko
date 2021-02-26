a5dada7401e800c382f6cb9ddc9848e60cb0c83c
feiz <logiq.feiz@gmail.com>
2021-02-26 15:04:32 +0900

    dynamodb localテスト環境を構築中

--- serverless.ts
+++ serverless.ts
@@ -9,9 +9,13 @@ const serverlessConfiguration: AWS = {
     webpack: {
       webpackConfig: './webpack.config.js',
       includeModules: true
+    },
+    dynamodb: {
+      stages: ['dev']
     }
+
   },
-  plugins: ['serverless-offline', 'serverless-webpack'],
+  plugins: ['serverless-offline', 'serverless-webpack', 'serverless-dynamodb-local'],
   provider: {
     name: 'aws',
     runtime: 'nodejs12.x',
@@ -24,10 +28,56 @@ const serverlessConfiguration: AWS = {
       SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET,
       SLACK_BOT_TOKEN: process.env.SLACK_BOT_TOKEN
     },
-    lambdaHashingVersion: '20201221'
+    lambdaHashingVersion: '20201221',
+    iam: {
+      role: {
+        statements: [
+          {
+            Effect: 'Allow',
+            Action: [
+              'dynamodb:*'
+            ],
+            Resource: [
+              { 'Fn::Join': [':', ['arn:aws:dynamodb', { Ref: 'AWS::Region' }, { Ref: 'AWS::AccountId' }, 'table/Saying']] },
+              { 'Fn::Join': [':', ['arn:aws:dynamodb', { Ref: 'AWS::Region' }, { Ref: 'AWS::AccountId' }, 'table/Word']] }
+            ]
+          }
+
+        ]
+      }
+    }
   },
   // import the function via paths
-  functions: { slack }
+  functions: { slack },
+  resources: {
+    Resources: {
+      saying: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "Saying",
+          BillingMode: 'PAY_PER_REQUEST',
+          AttributeDefinitions: [{ AttributeName: "keyword", AttributeType: "S" }],
+          KeySchema: [{ AttributeName: "keyword", KeyType: "HASH" }]
+        }
+      },
+      word: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "Word",
+          BillingMode: 'PAY_PER_REQUEST',
+          AttributeDefinitions: [
+              { AttributeName: "keyword", AttributeType: "S" },
+              { AttributeName: "word", AttributeType: "S" }
+          ],
+          KeySchema: [
+              { AttributeName: "keyword", KeyType: "HASH" },
+              { AttributeName: "word", KeyType: "RANGE" }
+          ]
+        }
+      }
+
+    }
+  }
 }
 
 module.exports = serverlessConfiguration
