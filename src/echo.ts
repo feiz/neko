fea62fd4e7667b652e518f7e930bc4de72bf8a5a
feiz <logiq.feiz@gmail.com>
2021-03-13 00:30:43 +0900

    echoが広範囲に反応してしまうのでマッチしにくくする

--- src/echo.ts
+++ src/echo.ts
@@ -1,7 +1,7 @@
 import { App } from '@slack/bolt'
 
 export default (app: App) => {
-  app.message('echo', async ({ message, say }): Promise<void> => {
-    await say('私は卑しい豚です')
+  app.message(/^\?echo$/, async ({ context, say }): Promise<void> => {
+    await say(':hispeed_sharkdance:')
   })
 }
