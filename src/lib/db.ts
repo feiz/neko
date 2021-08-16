0462d5d384e24b0e421c30e1732ce0de042903ff
feiz <logiq.feiz@gmail.com>
2021-08-17 07:21:57 +0900

    local dynamoでテストする設定

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,16 @@
-import { DynamoDB, DynamoDBConfiguration } from "@aws-sdk/client-dynamodb-v2-node";
+import { DynamoDB, DynamoDBConfiguration } from '@aws-sdk/client-dynamodb-v2-node'
 
-let config: DynamoDBConfiguration = { region: process.env.AWS_DEFAULT_REGION }
+const config: DynamoDBConfiguration = { region: process.env.AWS_DEFAULT_REGION }
+
 if (process.env.AWS_DYNAMODB_ENDPOINT) {
-    config.endpoint = process.env.AWS_DYNAMODB_ENDPOINT;
+  config.endpoint = process.env.AWS_DYNAMODB_ENDPOINT
 }
-const dynamoDB = new DynamoDB(config);
 
-export { dynamoDB };
+if (process.env.JEST_WORKER_ID) {
+  config.endpoint = 'http://localhost:8000'
+  config.sslEnabled = false
+  config.region = 'local-env'
+}
+const dynamoDB = new DynamoDB(config)
+
+export { dynamoDB }
